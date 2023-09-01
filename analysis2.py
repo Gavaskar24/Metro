7ccb515eff8ddab3075d4752859210da41f05607
Gavaskar Muppavaram <117184742+Gavaskar24@users.noreply.github.com>
2023-09-02 00:47:48 +0530

    Question till 13 are done

--- analysis2.py
+++ analysis2.py
@@ -30,6 +30,8 @@ df['TWO_WHEELER'] = pd.to_numeric(df['TWO_WHEELER'], errors='coerce', downcast='
 df['BICYCLES'] = pd.to_numeric(df['BICYCLES'], errors='coerce', downcast='integer')
 df['OTHERS'] = pd.to_numeric(df['OTHERS'], errors='coerce', downcast='integer')
 
+# NOTE: Here Nan values are replaced by -1
+df['TWO_WHEELER'] = df['TWO_WHEELER'].fillna(-1).astype(int)
 
 
 
@@ -42,9 +44,9 @@ df['O_TYPE'] = df['O_TYPE'].str.extract(r'(\w+)', expand=False)
 df['ACCESS_MODE'] = df['ACCESS_MODE'].str.extract(r'(\w+)', expand=False)
 df['EGRESS_MODE'] = df['EGRESS_MODE'].str.extract(r'(\w+)', expand=False)
 df['D_TYPE'] = df['D_TYPE'].str.extract(r'(\w+)', expand=False)
-df['ACCESS_DISTANCE'] = df['ACCESS_DISTANCE'].str.extract(r'(\w+)', expand=False)
-df['EGRESS_DISTANCE'] = df['EGRESS_DISTANCE'].str.extract(r'(\w+)', expand=False)
-df['BICYCLE_USE'] = df['BICYCLE_USE'].str.extract(r'(\w+)', expand=False)
+df['ACCESS_DISTANCE'] = df['ACCESS_DISTANCE'].str.replace(r'\(.*\)', '',regex=True)
+df['EGRESS_DISTANCE'] = df['EGRESS_DISTANCE'].str.replace(r'\(.*\)', '',regex=True)
+df['BICYCLE_USE'] = df['BICYCLE_USE'].str.replace(r'\(.*\)', '',regex=True)
 df['PASS'] = df['PASS'].str.extract(r'(\w+)', expand=False)
 df['CROWDING'] = df['CROWDING'].str.extract(r'(\w+)', expand=False)
 df['GENDER']=df['GENDER'].str.extract(r'(\w+)', expand=False)
@@ -61,41 +63,100 @@ df['S_3'] = df['S_3'].str.replace(r'\(.*?\)', '', regex=True)
 
 arr=df['PARKING'].unique()
 
-print(arr)
 
-
+###############    PARKING COLUMN ##############
 #Creating a new column PARK
 df['PARK']=np.nan
 
 #Using the array of arr, convert the strings of column PARKING into integer values 
 # and store then in new column called PARK
 for i in range(len(arr)):
-    df.loc[df['PARKING'] == arr[i], 'PARK'] = i
+    df.loc[df['PARKING'] == arr[i], 'PARK'] = i+1
 
 # Converting floats to integers in PARK and TWO_WHEELERcolumn
 df['PARK']=df['PARK'].astype(int)
-mask=df['TWO_WHEELER'].notna()
-df['TWO_WHEELER']=df.loc[mask,'TWO_WHEELER'].astype('int64')
 
-print(df.dtypes)
+#################################################
 
 
+############## DESTINATION TYPE #################
 #using the array of destination_types, convert the strings of column D_TYPE into integer values not float values
 # and store then in new column called DESTINATION
 
 #Creating destination_types array
-destination_types=df['D_TYPE'].unique()
+destination_types=['Home','Work','School ','Shopping','Restaurant','Social','Friend','Other']
 
 #creating a dictionary to map destination_type to integers
 destination_mapping = {dest: 1+i for i, dest in enumerate(destination_types)}
 
 # Map 'D_TYPE' column to integers and fill unmatched types with -1
 df['DESTINATION'] = df['D_TYPE'].map(destination_mapping).fillna(0).astype(int)
-# df['DESTINATION'].astype(int)
-print(df['DESTINATION'].unique())
-print(destination_types)
-#Create a new column DESTINATION with 
+
+###################################################
+
+############## ORIGIN TYPE #################
+#using the array of destination_types, convert the strings of column D_TYPE into integer values not float values
+# and store then in new column called DESTINATION
+
+#Creating destination_types array
 
 
+origin_types=['Home','Work','School ','Shopping','Restaurant','Social','Friend','Other']
 
+#creating a dictionary to map destination_type to integers
+origin_mapping = {org: 1+i for i, org in enumerate(origin_types)}
+
+# Map 'D_TYPE' column to integers and fill unmatched types with -1
+df['ORIGIN'] = df['O_TYPE'].map(origin_mapping).fillna(0).astype(int)
+
+###################################################
+
+
+############## TRIP SEQUENCE #################
+
+#Creating sequence array, considered rest of other sequences as 0
+sequence=['Metro ','Bus + Metro ','Metro + Bus ','Bus1 + Metro + Bus2 ','Metro1 + Bus + Metro2 ']
+
+#Initiate new array
+df['TRIP']=0
+
+# Map 'TRIP_SEQUENCE' column to integers 
+for i, value in enumerate(sequence, start=1):
+    df.loc[df['TRIP_SEQUENCE'] == value, 'TRIP'] = i
+
+###################################################
+
+
+###############  ACCESS_DISTANCE and EGRESS_DISTANCE ##########################
+access=['< 0.5 km ', '1.5 km to 2 km ',    '0.5 to 1 km ', '1 km to 1.5 km ',
+ '3 km to 3.5 km ', '4.5 km to 5 km ', '2.5 km to 3 km ',         '> 5 km ',
+ '3.5 km to 4 km ', '4 km to 4.5 km ', '2 km to 2.5 km ']
+
+df['ACCESS_DIS']=0
+df['EGRESS_DIS']=0
+
+access_mapping = {org: 1+i for i, org in enumerate(access)}
+
+# Map 'D_TYPE' column to integers and fill unmatched types with -1
+df['ACCESS_DIS'] = df['ACCESS_DISTANCE'].map(access_mapping).fillna(0).astype(int)
+df['EGRESS_DIS'] = df['EGRESS_DISTANCE'].map(access_mapping).fillna(0).astype(int)
+
+########################################################
+
+################# BICYCLE USE ####################
+
+bicycle=['For neither access nor egress trips ',
+                'For access trip only ',
+    'For both access and egress trips ',
+                'For egress trip only ']
+
+df['BICYCLE']=0
+
+bicycle_mapping={c: 1+i for i,c in enumerate(bicycle)}
+
+df['BICYCLE']=df['BICYCLE_USE'].map(bicycle_mapping).fillna(0).astype(int)
+
+#########################################
+
+print(df['PASS'].unique())
 
